f8f851a740d93caa478d8891cbeb36436fd6b87b
daniel1-2 <daniel263k@gmail.com>
2023-04-14 16:06:21 -0500

    Playing around with resizing with and without bounds

--- Data_Augmentations.py
+++ Data_Augmentations.py
@@ -1,20 +1,29 @@
 import tensorflow as tf
 import os
 import random
+import cv2 as cv2
+import matplotlib.pyplot as plt
+import numpy as np
 import keras
 from tensorflow.keras import layers
 
 training_data_path = "data_archive/asl_alphabet_train"
-
-
-for dirlist in os.listdir(training_data_path):
-    for root, directories, filenames in os.walk(os.path.join(training_data_path, dirlist)):
-        print("Inside Folder", dirlist, "Consist :", len(filenames), "Imageset")
-        for filename in filenames:
-          if random.randint() % 2 == 0:
-            IMG_WIDTH = random.randint(50, 1080)
-            IMG_LENGTH= random.randint(50, 1080)
-
-          resize_and_rescale = tf.keras.Sequential([
-            layers.Resizing(IMG_LENGTH, IMG_WIDTH),
-            layers.Rescaling(1./255)])
+image = cv2.imread("A1test.jpg")
+IMG_WIDTH = random.randint(1, 800)
+IMG_LENGTH= random.randint(1, 800)
+resized = tf.image.resize_with_pad(image, 100, 100)
+resized = np.squeeze(resized)
+plt.imsave("resized12.png", resized)
+resized_big = tf.image.resize_with_pad(image, 50, 50)
+resized_big = np.squeeze(resized_big)
+plt.imsave("resized_big12.png", resized_big)
+# for dirlist in os.listdir(training_data_path):
+#     for root, directories, filenames in os.walk(os.path.join(training_data_path, dirlist)):
+#         print("Inside Folder", dirlist, "Consist :", len(filenames), "Imageset")
+#         for filename in filenames:
+#             image_path = os.path.join(root, filename)
+#             image = cv.imread(image_path)
+#             if random.randint() % 2 == 0:
+#                 IMG_WIDTH = random.randint(50, 1080)
+#                 IMG_LENGTH= random.randint(50, 1080)
+#                 resize_and_rescale = layers.Resizing(image, IMG_LENGTH, IMG_WIDTH)
